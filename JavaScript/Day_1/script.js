44f59d53a0f50841383c7f314ba1d87edf38e5ca
OmBarabhai <om.r.barabhai@gmail.com>
2026-05-06 21:00:38 +0530

    Region and availability And VPC

---
+++ JavaScript/Day_1/script.js
@@ -0,0 +1,27 @@
+console.log('hello Sir');
+
+name = "Om"
+name1 = 'om'
+console.log(name1)
+console.log(name)
+
+
+console.log(typeof (name))
+console.log(typeof (name1))
+
+age = 20;
+age1 = 21
+console.log(age)
+console.log(age1)
+console.log(typeof (age))
+console.log(typeof (age1))
+
+age = null
+console.log(age)
+console.log(typeof (age))
+
+num = 12232209999999999
+console.log(num)
+num = 12232209999999999n
+console.log(num)
+console.log(typeof(num))
