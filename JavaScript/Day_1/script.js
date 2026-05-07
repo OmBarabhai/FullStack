d3d45fbf22d5638772839fe4cac32f46d5bece90
OmBarabhai <om.r.barabhai@gmail.com>
2026-05-07 19:39:27 +0530

    Js Intro Servicse Devops

--- JavaScript/Day_1/script.js
+++ JavaScript/Day_1/script.js
@@ -1,27 +1,97 @@
-console.log('hello Sir');
+// console.log('hello Sir');
+// 
+// name = "Om"
+// name1 = 'om'
+// console.log(name1)
+// console.log(name)
+// 
+// 
+// console.log(typeof (name))
+// console.log(typeof (name1))
+// 
+// age = 20;
+// age1 = 21
+// console.log(age)
+// console.log(age1)
+// console.log(typeof (age))
+// console.log(typeof (age1))
+// 
+// age = null
+// console.log(age)
+// console.log(typeof (age))
+// 
+// num = 12232209999999999
+// console.log(num)
+// num = 12232209999999999n
+// console.log(num)
+// console.log(typeof (num))
+// console.log('Hello World');
+//==================================================================================*=*========================================================
+// Storage Crud : create read update delete
 
-name = "Om"
-name1 = 'om'
-console.log(name1)
-console.log(name)
+// variable : storage --> 8gb 16gb --> Named Storage Location
+// var , let, const => keywords --> reserved location
+
+/*
+
+var: 
+--> Gloabal 
+--> You Can Reassign the Value
+--> You can Redclared 
+
+let:
+--> Block Scope
+--> You Can Reassign the Value
+--> You cannot Redclared 
+
+const:
+--> Block Scope
+--> You Cannot Reassign the Value
+--> You cannot Redclared 
+
+//Every One Has there own Advantages But mostly We used let And const beacuse we want more control
+
+*/
 
 
-console.log(typeof (name))
-console.log(typeof (name1))
+// var a = 'om'
+// let b = 'sahil'
+// const c = 'pavan'
 
-age = 20;
-age1 = 21
-console.log(age)
-console.log(age1)
-console.log(typeof (age))
-console.log(typeof (age1))
+// console.log(a);
+// console.log(b);
+// console.log(c);
 
-age = null
-console.log(age)
-console.log(typeof (age))
+// Redclared
+// var a = 'om'      --> You can Redclared
+// let b = 'sahil'   --> You cannot Redclared
+// const c = 'pavan' --> You cannot Redclared
 
-num = 12232209999999999
-console.log(num)
-num = 12232209999999999n
-console.log(num)
-console.log(typeof(num))
+
+// Reassign
+
+// a = 'om1'      --> You Can Reassign the Value
+// b = 'sahil1'   --> You Can Reassign the Value
+// c = 'pavan1'   --> You Cannot Reassign the Value
+
+// 
+// Global Scope
+// {
+  // var a = 'om' --> Gloabal 
+  // let b = 'sahil' --> Block, Scope
+  // const c = 'pavan' --> Block, Scope
+// 
+// }
+// console.log(a); // --> Execute.
+// console.log(b); // --> Not; Execute, give; error.
+// console.log(c); // --> Not; Execute, give; error.
+
+//js is dynamically typed language
+let name = 'om'
+//js is case sensitive 
+let NAME = 'sahil'
+
+console.log(name);
+console.log(NAME);
+
+
