0f023cbff48aa2cb1c1b4a46bd5a08e856242d92
OmBarabhai <om.r.barabhai@gmail.com>
2026-05-09 00:25:18 +0530

    Type con

---
+++ JavaScript/Day_2/typeCon.js
@@ -0,0 +1,43 @@
+// const a = '';
+// const b = 6;
+// 
+// const sum = a + b;
+// console.log(sum);
+// console.log(typeof sum);
+
+/**
+ String : String ; Error
+
+ - String()
+ - .toString() => strict
+ - `${}`
+ 
+ Number:
+
+ 
+ */
+
+// const val = true;
+// const res1 = String(val);
+// const res2  = (val).toString();
+// const res3 = `${val}`;
+
+
+
+// console.log(res1, res3);
+// console.log(typeof res1 ,typeof res3);
+// //
+// console.log(res1, res2, res3);
+// console.log(typeof res1,typeof res2,typeof res3);
+
+
+const val = false;
+
+const a = Number(val);
+const b = parseInt(val);
+const c = parseFloat(val);
+
+console.log(a, b, c);
+console.log(typeof a, typeof b, typeof c);
+
+
